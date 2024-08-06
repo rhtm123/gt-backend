08957258d9651017b86eca56f8bbba43354aed83
rhtm123 <maurya.iitk@gmail.com>
2024-08-06 20:13:32 +0530

    v0.0.5

--- gtbackend/settings_prod.py
+++ gtbackend/settings_prod.py
@@ -4,17 +4,17 @@ from gtbackend.settings import *
 
 ALLOWED_HOSTS += ["*"]
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DATABASE_NAME'),
-#         'USER': config('DATABASE_USER'),
-#         'PASSWORD': config('DATABASE_PASSWORD'),
-#         'HOST': config('DATABASE_HOST'),
-#         'PORT': config('DATABASE_PORT'),
-#         'OPTIONS': {'sslmode': 'require'},
-#         }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
+        'OPTIONS': {'sslmode': 'require'},
+        }
+}
 
 # CLOUDINARY_STORAGE = {
 #     'CLOUD_NAME': config('CLN_CLOUD_NAME'),
